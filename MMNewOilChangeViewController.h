51d87c097459dcaaed8383369b9400cf9e200980
Emilian Parvanov <empar@mail.bg>
2014-02-18 18:26:30 +0200

    boza i bani4ka 3

---
+++ MMNewOilChangeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMNewOilChangeViewController.h
+//  iCars
+//
+//  Created by Emilian Parvanov on 2/18/14.
+//  Copyright (c) 2014 MMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMNewOilChangeViewController : UIViewController
+
+@end
---
+++ MMNewServiceViewController.m
@@ -0,0 +1,38 @@
+//
+//  MMNewServiceViewController.m
+//  iCars
+//
+//  Created by Emilian Parvanov on 2/18/14.
+//  Copyright (c) 2014 MMA. All rights reserved.
+//
+
+#import "MMNewServiceViewController.h"
+
+@interface MMNewServiceViewController ()
+
+@end
+
+@implementation MMNewServiceViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
